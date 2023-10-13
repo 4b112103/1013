758f8122b53d439ad38fecfb059bf1a58ee31f7a
4B112103 許大成 <125719751+4b112103@users.noreply.github.com>
2023-10-13 02:21:34 +0000

    1021

--- week05/prob01/main.py
+++ week05/prob01/main.py
@@ -1,5 +1,12 @@
-#! /usr/bin/env python3
+import math
 
-# write your solution here
+x1, y1, z1 = map(int, input().split())
+x2, y2, z2 = map(int, input().split())
+
+d = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+d = round(d, 2)
+
+print(d)
 
 
--- week05/prob02/main.py
+++ week05/prob02/main.py
@@ -1,3 +1,3 @@
 #! /usr/bin/env python3
 
-# Write your code here
+# 遞迴
