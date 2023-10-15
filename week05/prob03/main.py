7ff89fb6304fdefa9b53567dc8c596c703bee9c6
4B112103 許大成 <125719751+4b112103@users.noreply.github.com>
2023-10-15 13:17:24 +0000

    prob04

--- week05/prob03/main.py
+++ week05/prob03/main.py
@@ -1,3 +1,23 @@
 #! /usr/bin/env python3
 
 # Write your code here
+def main(R,C):
+    num = 1
+    matrix = [[0] * C for _ in range(R)]
+    for i in range (C):
+        for j in range(R):
+            matrix[j][i] = num
+            num+=1 
+
+    return matrix
+
+def print_m(matrix):
+    for i in matrix:
+        for num in i:
+            print(num,end=" ")
+        print()
+
+if __name__=="__main__":
+    R, C = map(int, input().split())
+    nm = main(R,C)
+    print_m(nm)
--- week05/prob04/main.py
+++ week05/prob04/main.py
@@ -1,3 +1,28 @@
 #! /usr/bin/env python3
 
 # Write your code here
+def variance(numbers):
+    # Check if the input list has fewer than 2 elements and return None
+    if len(numbers) < 2:
+        return None
+    
+    # Calculate the mean (average) of the numbers
+    mean = sum(numbers) / len(numbers)
+    
+    # Calculate the squared differences and store them in a list
+    squared_differences = [(x - mean) ** 2 for x in numbers]
+    
+    # Calculate the variance using the formula
+    variance = sum(squared_differences) / (len(numbers) - 1)
+    
+    return variance
+
+# Input the list of integers
+input_numbers = [int(x) for x in input().split()]
+
+if __name__ == "__main__":
+    result = variance(input_numbers)
+    if result is not None:
+        print(result)
+    else:
+        print(None)
