114637f15086b80d9bb15932928b561fc4b18f71
4B112103 許大成 <125719751+4b112103@users.noreply.github.com>
2023-10-15 12:27:03 +0000

    week05 prob02

--- week05/prob02/main.py
+++ week05/prob02/main.py
@@ -1,7 +1,8 @@
 def power(a,n):
-    an = a
-    for i in range(1,n-1):
-        an = a*
-    return an
+    if n == 0:
+        return 1;
+    else:
+        return a * power(a, n-1)
+
 
 print(power(int(input()),int(input())))
