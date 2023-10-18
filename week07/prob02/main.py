e1476d64c2a64650aef43834d9e559ac4a2c46a1
4B112103 許大成 <125719751+4b112103@users.noreply.github.com>
2023-10-18 13:13:31 +0000

    prob 02

--- week07/prob02/main.py
+++ week07/prob02/main.py
@@ -5,7 +5,20 @@ def sign_pairs(numbers):
     #---------------------- 
     # write your code here
     #---------------------- 
+    num_list = [int(num) for num in numbers.split()]
 
+    result = []
+
+    i = 0
+    while i < len(num_list) - 1:
+        pair = [num_list[i], num_list[i + 1]]
+        if (pair[0] > 0 and pair[1] > 0) or (pair[0] < 0 and pair[1] < 0):
+            result.append(pair)
+            i += 2  
+        else:
+            i += 1
+
+    return result
 
 #---------------------------------------------------------------------
 # you can modify the following to test your code more thoroughly
