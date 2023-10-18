22c4f96c0de78b494ea1d11179e0bbe29982df64
4B112103 許大成 <125719751+4b112103@users.noreply.github.com>
2023-10-18 13:11:57 +0000

    week07 prob01

---
+++ week07/prob01/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+def sign_pairs(numbers):
+    pass
+    #---------------------- 
+    # write your code here
+    #---------------------- 
+    num_list = [int(num) for num in numbers.split()]
+
+    # Initialize an empty list to store adjacent pairs with the same sign
+    result = []
+
+    # Iterate through the list to find adjacent pairs
+    for i in range(len(num_list) - 1):
+        if (num_list[i] > 0 and num_list[i + 1] > 0) or (num_list[i] < 0 and num_list[i + 1] < 0):
+            result.append([num_list[i], num_list[i + 1]])
+
+    return result
+#---------------------------------------------------------------------
+# you can modify the following to test your code more thoroughly
+#---------------------------------------------------------------------
+data = [
+    "1 -2 3 4 -5 6 7 8 -9 10",
+    "1 -2 3 -5 6 -7 8 -9 10",
+    "1 2 3 4 5 6 7 8 9 10",
+    "1 -2 3 -4 5 -6 7 -8 9 -10",
+    "1 -2 3 -4 5 -6 7 -8 9 -10 11",
+    "1 -2 3 -4 5 -6 7 -8 9 -10 11 -12",
+    "-1 -2 -3 -4 -5 -6 -7 -8 -9 -10",
+ ]
+
+        
+def main():
+    global data
+    for test_case in data:
+        result = sign_pairs(test_case)
+        print(result)
+        
+if __name__ == '__main__':
+    main()
